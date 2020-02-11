ab5b086d13c63025b81e3dec1dae2afe0fc4570f
NemecJaroslav <nemec.jaroslav88@seznam.cz>
2020-02-11 21:47:50 +0100

    Reworked tests.

--- production_code/non_trout/tests/test_mrs_parser.py
+++ production_code/non_trout/tests/test_mrs_parser.py
@@ -1,62 +1,36 @@
 from unittest import TestCase
 from production_code.common.mrs_parser import GPSCoordinate
 from production_code.non_trout.non_trout_parser import NonTroutParser
-from production_code.non_trout.tests.test_data import (get_expected_locations_url, get_expected_location_ids,
-                                                       get_expected_location_names, get_expected_gps,
-                                                       get_expected_headquarters, get_expected_area)
+from production_code.non_trout.tests.test_data import get_expected_fishing_locations
 
 
 class TestMRSParser(TestCase):
     def setUp(self):
         self.parser = NonTroutParser()
+        self.parser.parse()
 
-    def test_get_locations_url(self):
-        expected_locations_url = get_expected_locations_url()
-        self.assertEqual(245, len(expected_locations_url))
-        self.assertEqual(sorted(expected_locations_url),
-                         sorted(set(expected_locations_url)))
-        self.assertEqual(sorted(expected_locations_url),
-                         sorted(self.parser._get_locations_url()))
+    def test_parse(self):
+        expected_fishing_locations = get_expected_fishing_locations()
+        actual_fishing_locations = self.parser._fishing_locations
 
-    def test_get_location_id(self):
-        expected_results = get_expected_location_ids()
-        self.assertEqual(245, len(expected_results))
-        locations = [expected_result[0] for expected_result in expected_results]
-        ids = [expected_result[1] for expected_result in expected_results]
-        self.assertEqual(sorted(locations),
-                         sorted(set(locations)))
-        self.assertEqual(sorted(ids),
-                         sorted(set(ids)))
-        for expected_result in expected_results:
-            context = self.parser._get_decoded_source_page(expected_result[0])
-            actual = self.parser._get_location_id(context)
-            self.assertEqual(expected_result[1], actual)
+        expected_fishing_locations.sort(key=lambda x: x.identifier)
+        actual_fishing_locations.sort(key=lambda x: x.identifier)
 
-    def test_get_location_name(self):
-        expected_results = get_expected_location_names()
-        self.assertEqual(245, len(expected_results))
-        locations = [expected_result[0] for expected_result in expected_results]
-        names = [expected_result[1] for expected_result in expected_results]
-        self.assertEqual(sorted(locations),
-                         sorted(set(locations)))
-        self.assertEqual(sorted(names),
-                         sorted(set(names)))
-        for expected_result in expected_results:
-            context = self.parser._get_decoded_source_page(expected_result[0])
-            actual = self.parser._get_location_name(context)
-            self.assertEqual(expected_result[1], actual)
-
-    def test_get_gps(self):
-        expected_results = get_expected_gps()
-        self.assertEqual(245, len(expected_results))
-        locations = [expected_result[0] for expected_result in expected_results]
-        self.assertEqual(sorted(locations),
-                         sorted(set(locations)))
-        for expected_result in expected_results:
-            context = self.parser._get_decoded_source_page(expected_result[0])
-            actual = self.parser._get_gps(context)
-            self.assertEqual(sorted(expected_result[1]),
-                             sorted(actual))
+        self.assertEqual(len(expected_fishing_locations),
+                         len(actual_fishing_locations))
+        for (expected_fishing_location,
+             actual_fishing_location) in zip(expected_fishing_locations,
+                                             actual_fishing_locations):
+            self.assertEqual(expected_fishing_location.identifier,
+                             actual_fishing_location.identifier)
+            self.assertEqual(expected_fishing_location.name,
+                             actual_fishing_location.name)
+            self.assertEqual(expected_fishing_location.headquarter,
+                             actual_fishing_location.headquarter)
+            self.assertEqual(expected_fishing_location.area,
+                             actual_fishing_location.area)
+            # self.assertEqual(expected_fishing_location.gps_locations,
+            #                  actual_fishing_location.gps_locations)
 
     # TODO: add more test cases & uniqueness test
     def test_convert_string_to_gps(self):
@@ -103,25 +77,3 @@ class TestMRSParser(TestCase):
         for expected_result in expected_results:
             self.assertEqual(expected_result[1],
                              self.parser._convert_string_to_gps(expected_result[0]))
-
-    def test_get_headquarter(self):
-        expected_results = get_expected_headquarters()
-        self.assertEqual(245, len(expected_results))
-        locations = [expected_result[0] for expected_result in expected_results]
-        self.assertEqual(sorted(locations),
-                         sorted(set(locations)))
-        for expected_result in expected_results:
-            context = self.parser._get_decoded_source_page(expected_result[0])
-            actual = self.parser._get_headquarter(context)
-            self.assertEqual(expected_result[1], actual)
-
-    def test_get_area(self):
-        expected_results = get_expected_area()
-        self.assertEqual(245, len(expected_results))
-        locations = [expected_result[0] for expected_result in expected_results]
-        self.assertEqual(sorted(locations),
-                         sorted(set(locations)))
-        for expected_result in expected_results:
-            context = self.parser._get_decoded_source_page(expected_result[0])
-            actual = self.parser._get_area(context)
-            self.assertEqual(expected_result[1], actual)
