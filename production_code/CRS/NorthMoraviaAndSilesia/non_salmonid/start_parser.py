be1d19d7d6a9e0ba405db9d9302d4a98efabbb37
jnemec <jaroslav.nemec@fei.com>
2020-03-13 19:07:49 +0100

    start_parser added for each parser

--- production_code/CRS/NorthMoraviaAndSilesia/non_salmonid/start_parser.py
+++ production_code/CRS/NorthMoraviaAndSilesia/non_salmonid/start_parser.py
@@ -1,5 +1,5 @@
 from production_code.common.distance_limit import DistanceLimit
-from production_code.MRS.salmonid.salmonid_parser import SalmonidParser
+from production_code.CRS.NorthMoraviaAndSilesia.non_salmonid.parser import NorthMoraviaAndSilesiaNonSalmonidParser
 
 # Marek
 # 49.1686025, 16.5705978
@@ -13,13 +13,13 @@ from production_code.MRS.salmonid.salmonid_parser import SalmonidParser
 # 49.4823661, 16.8364286
 
 if __name__ == "__main__":
-    parser = SalmonidParser()
+    parser = NorthMoraviaAndSilesiaNonSalmonidParser()
     parser.parse()
 
     parser.print_all_headquarters_and_their_locations()
     parser.print_suitable_fishing_locations((49.1806731, 16.6801281), DistanceLimit(0., 30.))
     parser.print_all_headquarters_and_their_areas()
     parser.print_suspiciously_close_gps_locations(DistanceLimit(0.0, 0.5))
-    parser.print_all_headquarters_and_distance_to_their_locations()
-    parser.print_fishing_summary_given_by_id(["463032", "463064"])
-    parser.print_fishing_summary_given_by_name(["RYBNICKÝ POTOK 1", "KLANEČNICE 1"])
+    # parser.print_all_headquarters_and_distance_to_their_locations()
+    parser.print_fishing_summary_given_by_id(["471046", "471047"])
+    parser.print_fishing_summary_given_by_name(["MORAVA 16", "MORAVA 15"])
--- production_code/CRS/NorthMoraviaAndSilesia/salmonid/start_parser.py
+++ production_code/CRS/NorthMoraviaAndSilesia/salmonid/start_parser.py
@@ -1,5 +1,5 @@
 from production_code.common.distance_limit import DistanceLimit
-from production_code.MRS.salmonid.salmonid_parser import SalmonidParser
+from production_code.CRS.NorthMoraviaAndSilesia.salmonid.parser import NorthMoraviaAndSilesiaSalmonidParser
 
 # Marek
 # 49.1686025, 16.5705978
@@ -13,13 +13,13 @@ from production_code.MRS.salmonid.salmonid_parser import SalmonidParser
 # 49.4823661, 16.8364286
 
 if __name__ == "__main__":
-    parser = SalmonidParser()
+    parser = NorthMoraviaAndSilesiaSalmonidParser()
     parser.parse()
 
     parser.print_all_headquarters_and_their_locations()
     parser.print_suitable_fishing_locations((49.1806731, 16.6801281), DistanceLimit(0., 30.))
     parser.print_all_headquarters_and_their_areas()
     parser.print_suspiciously_close_gps_locations(DistanceLimit(0.0, 0.5))
-    parser.print_all_headquarters_and_distance_to_their_locations()
-    parser.print_fishing_summary_given_by_id(["463032", "463064"])
-    parser.print_fishing_summary_given_by_name(["RYBNICKÝ POTOK 1", "KLANEČNICE 1"])
+    # parser.print_all_headquarters_and_distance_to_their_locations()
+    parser.print_fishing_summary_given_by_id(["473024", "473029"])
+    parser.print_fishing_summary_given_by_name(["DESNÁ 3", "BYSTŘICE HANÁCKÁ 2 "])
