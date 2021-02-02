07bceec87e77ee481ffafcf06bce6768dc9e146a
jaroslav.nemec <jaroslav.nemec@thermofisher.com>
2021-02-02 20:06:45 +0100

    webpage changes

--- production_code/CRS/NorthMoraviaAndSilesia/common/constants.py
+++ production_code/CRS/NorthMoraviaAndSilesia/common/constants.py
@@ -6,9 +6,9 @@ class NorthMoraviaAndSilesiaCommonConstants:
     LOCATION_ID_PATTERN = "(?P<" + Constants.LOCATION_ID_PATTERN_GROUP_NAME + ">47\\d\\s*\\d{3})"
     LOCATION_NAME_PATTERN = ("<title>.*\\|.*\\|" + "(?P<"
                              + Constants.LOCATION_NAME_PATTERN_GROUP_NAME
-                             + ">.*)[–-].*(MO|VÚS).*</title>")
+                             + ">.*)[–-].*(MO|VÚS|ÚS).*</title>")
     HEADQUARTER_PATTERN = ("<title>.*\\|.*\\|.*[–-](?P<"
-                           + Constants.HEADQUARTER_PATTERN_GROUP_NAME + ">.*(MO|VÚS).*)</title>")
+                           + Constants.HEADQUARTER_PATTERN_GROUP_NAME + ">.*(MO|VÚS|ÚS).*)</title>")
     AREA_PATTERN = ("<p><strong>(?:[\\d\\s,]*km)*\\s*(?P<"
                     + Constants.AREA_PATTERN_GROUP_NAME
                     + ">\\d[,\\d]*)\\s*ha\\s*(?:[\\d\\s,]*km)*</strong></p>")
